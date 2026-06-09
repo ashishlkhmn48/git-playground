a16b6db992d60dcd884454bbc5e8f91f06d7c11d
Ashish Lakhmani <31071974+ashishlkhmn48@users.noreply.github.com>
2026-06-09 21:53:44 +0530

    Updated Test.java from browser

--- Test.java
+++ Test.java
@@ -2,4 +2,8 @@ class Test{
 	public static void main(String[] args){
 		System.out.println("Hello Test");
 	}
+
+	public void func(){
+		System.out.println("I edited this func from browser");
+	}
 }
