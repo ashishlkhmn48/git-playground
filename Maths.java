86a24ef3d43bc5f998556a58459ff9a86b5f9bba
Ashish Lakhmani <ashishlkhmn48@gmail.com>
2026-06-13 18:35:20 +0530

    Added division method

--- Maths.java
+++ Maths.java
@@ -9,5 +9,12 @@ public class Maths {
 
   public static int multiply(int a, int b){
     return a*b;
+  }
+  
+  public static int divide(int a, int b){
+    if(b==0){
+      throw new Exception();
+    }
+    return a/b;
   } 
 }
